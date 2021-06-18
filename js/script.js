8a71eac961e401801d2dcb7e3bdbfab1b81fc632
saltywillams <70780931+JoelWillams2021@users.noreply.github.com>
2021-06-17 22:55:47 -0400

    Added an Imperial Calculator
    Can now add imperial values to the BMI Calculator

--- js/script.js
+++ js/script.js
@@ -7,10 +7,42 @@ function bmiCalculator(){
     if (interpretation <= 18.5){
       document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are underweight.";
     }
+
     else if (interpretation > 18.5 && interpretation <= 24.9){
       document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are normal.";
     }
-    else if (interpretation > 24.9){
+
+    else if (interpretation > 24.9  && interpretation <= 29.9 ){
       document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are overweight.";
     }
+
+    else if (interpretation > 29.9){
+      document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are obese.";
+    }
+
+}
+
+
+function bmiCalculatorSecond(){
+    var weight = document.getElementById("Weight_Second").value;
+    var height = document.getElementById("Height_Second").value;
+    var bmiInput = weight*703/ Math.pow(height,2);
+    bmiInput = Math.round(bmiInput);
+    var interpretation = bmiInput
+    if (interpretation <= 18.5){
+      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are underweight.";
+    }
+
+    else if (interpretation > 18.5 && interpretation <= 24.9){
+      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are normal.";
+    }
+
+    else if (interpretation > 24.9  && interpretation <= 29.9 ){
+      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are overweight.";
+    }
+
+    else if (interpretation > 29.9){
+      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are obese.";
+    }
+
 }
