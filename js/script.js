00801f9115126c6f34c956725c93e1b5c38ef0eb
Jason Li <jasonli.0616@outlook.com>
2021-06-21 21:25:20 -0400

    make code slightly clear

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
+function getWeightComment(interpretation) {
+    weightComment = ''
+    if (interpretation <= 18.5){
+        weightComment = 'underweight';
+    } else if (interpretation > 18.5 && interpretation <= 24.9){
+        weightComment = 'normal';
+    } else if (interpretation > 24.9  && interpretation <= 29.9 ){
+        weightComment = 'overweight';
+    } else if (interpretation > 29.9){
+        weightComment = 'obese';
+    }
+    return weightComment
+}
+
+
 function bmiCalculator(){
     var weight = document.getElementById("Weight").value;
     var height = document.getElementById("Height").value;
     var bmiInput = weight/ Math.pow(height,2);
     bmiInput = Math.round(bmiInput);
     var interpretation = bmiInput
-    if (interpretation <= 18.5){
-      document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are underweight.";
-    }
-
-    else if (interpretation > 18.5 && interpretation <= 24.9){
-      document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are normal.";
-    }
-
-    else if (interpretation > 24.9  && interpretation <= 29.9 ){
-      document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are overweight.";
-    }
-
-    else if (interpretation > 29.9){
-      document.getElementById("Output").innerHTML = "Your BMI is " + interpretation + ", so you are obese.";
-    }
+    let weightComment = getWeightComment(interpretation);
+    document.getElementById("Output").innerHTML = `Your BMI is " ${interpretation}, so you are ${weightComment}.`;
 
 }
 
@@ -29,20 +31,7 @@ function bmiCalculatorSecond(){
     var bmiInput = weight*703/ Math.pow(height,2);
     bmiInput = Math.round(bmiInput);
     var interpretation = bmiInput
-    if (interpretation <= 18.5){
-      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are underweight.";
-    }
-
-    else if (interpretation > 18.5 && interpretation <= 24.9){
-      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are normal.";
-    }
-
-    else if (interpretation > 24.9  && interpretation <= 29.9 ){
-      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are overweight.";
-    }
-
-    else if (interpretation > 29.9){
-      document.getElementById("Output_2").innerHTML = "Your BMI is " + interpretation + ", so you are obese.";
-    }
+    let weightComment = getWeightComment(interpretation);
+    document.getElementById("Output").innerHTML = `Your BMI is " ${interpretation}, so you are ${weightComment}.`;
 
 }
